9bd5698426004dcc79650be797bfc3ad5fd0eaae
Danilo Medeiros Dantas <danilomedeiros.dox@gmail.com>
2017-09-30 10:21:29 -0300

    Atualizado

--- Lab6/src/funcionario/Funcionario.java
+++ Lab6/src/funcionario/Funcionario.java
@@ -1,19 +1,23 @@
 package funcionario;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import filme.Filme;
 
-public class Funcionario {
+public abstract class Funcionario {
 	private String nome;
-	private TipoDeFuncionario statusDoFuncionario;
-	private Set<Funcao> funcao;
+	private TipoDeFuncionario funcaoDoFuncionario;
+	//private Set<Funcao> funcao;
+	private Map<String, Filme> filmografia;
 
-	public Funcionario(String nome, Set<Funcao> funcao, TipoDeFuncionario tipoDeFuncionario) {
+	public Funcionario(String nome,/* Set<Funcao> funcao,*/ TipoDeFuncionario tipoDeFuncionario) {
 		this.nome = nome;
-		this.funcao = funcao;
-		this.statusDoFuncionario = tipoDeFuncionario;
+		//this.funcao = funcao;
+		filmografia = new HashMap<>();
+		this.funcaoDoFuncionario = tipoDeFuncionario;
 	}
 
 	public void getFilmografia() {
@@ -25,10 +29,10 @@ public class Funcionario {
 	}
 
 	public TipoDeFuncionario getStatusDoFuncionario() {
-		return statusDoFuncionario;
+		return funcaoDoFuncionario;
 	}
 
-	public Set<String> getFuncao() {
+	/*public Set<String> getFuncao() {
 		Set<String> aux = new HashSet<>();
 
 		for (Funcao funcao : this.funcao) {
@@ -36,6 +40,6 @@ public class Funcionario {
 			aux.add(funcao.toString());
 		}
 		return aux;
-	}
+	}*/
 
 }
